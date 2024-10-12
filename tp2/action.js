85b7be138a5760641f3fca36021b72ac34f2d4b8
AMIAlyndaa <lynda.amia@edu.univ-paris13.fr>
2024-10-12 23:56:09 +0200

    tp2

--- tp2/action.js
+++ tp2/action.js
@@ -1,23 +1,24 @@
-window.onload = () => {
-    // https://developer.mozilla.org/fr/docs/Web/API/Window/load_event
-    const paramsString = document.location.search; // récupérer des données envoyées par le formulaire
-    const searchParams = new URLSearchParams(paramsString); // https://developer.mozilla.org/fr/docs/Web/API/URLSearchParams
+document.getElementById('inscriptionForm').addEventListener('submit', function (event) {
+  event.preventDefault(); 
+
+ 
+  const nom = document.getElementById('nom').value;
+  const prenom = document.getElementById('prenom').value;
+  const email = document.getElementById('email').value;
+  const dateNaissance = document.getElementById('date_naissance').value;
+  const sexe = document.querySelector('input[name="sexe"]:checked').value;
+  const pays = document.getElementById('pays').value;
+
   
-    // Iterating the search parameters
-    for (const param of searchParams) {
-      console.log(param);
-  
-      const elementId = param[0];
-      const elementValue = param[1];
-      const element = document.getElementById(elementId);
-  
-      if (element !== null) {
-        element.textContent = elementValue;
-      }
-      if (param[0] === "address") {
-        element.href = `https://www.google.com/maps/search/?api=1&query=${elementValue}`;
-      } else if (param[0] === "email") {
-        element.href = `mailto:${elementValue}?subject=Hello!&body=What's up?`;
-      }
-    }
-  };
+  const params = new URLSearchParams({
+    nom: nom,
+    prenom: prenom,
+    email: email,
+    date_naissance: dateNaissance,
+    sexe: sexe,
+    pays: pays,
+  });
+
+ 
+  window.location.href = 'action.html?' + params.toString();
+});
